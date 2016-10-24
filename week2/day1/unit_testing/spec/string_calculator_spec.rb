fc355d1424af785ca89272ee14b42bce05d186ed
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-10-24 11:39:39 -0400

    rspec test

--- week2/day1/unit_testing/spec/string_calculator_spec.rb
+++ week2/day1/unit_testing/spec/string_calculator_spec.rb
@@ -1,5 +1,16 @@
 require_relative("../lib/string_calculator")
 
 RSpec.describe StringCalculator do
-	
+
+	describe "#add" do 
+		it "adds two numbers separated by commas" do
+			the_calc = StringCalculator.new
+			# p the_calculator.add("7,7")  ==  14
+			expect(the_calc.add("7,7")).to eq(14)   
+
+			
+			# p the_calculator.add("8,4") == 12
+			expect(the_calc.add("8,4")).to eq(12)
+		end		
+	end	
 end
