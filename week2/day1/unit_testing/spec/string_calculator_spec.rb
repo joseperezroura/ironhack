bc06d0a279a610dbfad8e0bc909f9357255e73f6
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-10-24 12:49:22 -0400

    added let to rspec

--- week2/day1/unit_testing/spec/string_calculator_spec.rb
+++ week2/day1/unit_testing/spec/string_calculator_spec.rb
@@ -3,33 +3,33 @@ require_relative("../lib/string_calculator")
 RSpec.describe StringCalculator do
 
 	describe "#add" do 
-		before :each do
-			@the_calc = StringCalculator.new
+		let :the_calc do
+			StringCalculator.new
 		end
 
 
 		it "adds two numbers separated by commas" do
 			
 			# p the_calculator.add("7,7")  ==  14
-			expect(@the_calc.add("7,7")).to eq(14)   			
+			expect(the_calc.add("7,7")).to eq(14)   			
 			# p the_calculator.add("8,4") == 12
-			expect(@the_calc.add("8,4")).to eq(12)
+			expect(the_calc.add("8,4")).to eq(12)
 		end		
 		
 		it "returns the number for a single number" do
 		
 			# p the_calculator.add("6") == 6
-			expect(@the_calc.add("6")).to eq(6)
+			expect(the_calc.add("6")).to eq(6)
 		end
 
 		it "returns 0 for the empty string" do
-		
+			
 			# p the_calculator.add("") == 0
-			expect(@the_calc.add("")).to eq(0)
+			expect(the_calc.add("")).to eq(0)
 		end
 		it "returns 0 when no argument given" do
 			
-			expect(@the_calc.add()).to eq(0)
+			expect(the_calc.add()).to eq(0)
 		end
 	end	
 end
