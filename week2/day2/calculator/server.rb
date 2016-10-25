bafe4149135a0cdcbd76c44e8153cc1948d70172
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-10-25 16:55:26 -0400

    created -,*,/ files

--- week2/day2/calculator/server.rb
+++ week2/day2/calculator/server.rb
@@ -16,6 +16,46 @@ post "/calculate_add" do
   "#{first} + #{second} = #{result}"
 end
 
+
+get "/subtract" do
+  erb(:subtract)
+end
+
+post "/calculate_subtract" do
+  first = params[:first_number].to_f
+  second = params[:second_number].to_f
+  result = first - second
+  "#{first} - #{second} = #{result}"
+end
+
+get "/multiply" do
+  erb(:multiply)
+end
+
+post "/calculate_multiply" do
+  first = params[:first_number].to_f
+  second = params[:second_number].to_f
+  result = first + second
+  "#{first} * #{second} = #{result}"
+end
+
+get "/divide" do
+  erb(:divide)
+end
+
+post "/calculate_divide" do
+  first = params[:first_number].to_f
+  second = params[:second_number].to_f
+  result = first / second
+  "#{first} / #{second} = #{result}"
+end
+
+
+
+
+
+
+
 # post "/calculate_add" do
 #   "Params data: " + params.inspect
 # end
