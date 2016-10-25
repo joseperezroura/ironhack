2465bf467a1532c9bcf690221083076209f1919b
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-10-25 17:29:17 -0400

    completed calculator class

--- week2/day2/calculator/lib/calculator.rb
+++ week2/day2/calculator/lib/calculator.rb
@@ -1,3 +1,28 @@
 class Calculator
+	  # result = calc.add(first, second)
+
+	def add(x, y)
+
+		z = x + y
+		z
+	end
+
+	def subtract(x, y)
+		z = x - y
+
+	end
+
+	def multiply(x, y)
+
+		z = x * y
+
+	end
+
+	def divide(x, y)
+
+		z = x / y
+
+	end
+
 
 end
--- week2/day2/calculator/server.rb
+++ week2/day2/calculator/server.rb
@@ -5,63 +5,73 @@ get "/" do
   erb(:home)
 end
 
-get "/add" do
-  erb(:add)
+get "/functions" do
+  erb(:functions)
 end
 
-post "/calculate_add" do
+post "/calculate" do
   first = params[:first_number].to_f
   second = params[:second_number].to_f
-  result = first + second
-  "#{first} + #{second} = #{result}"
+
+  
+
+  calc = Calculator.new
+
+  result = calc.add(first, second)
+  result = calc.subtract(first, second)
+  result = calc.multiply(first, second)
+  result = calc.divide(first, second)
+
 end
 
-
-get "/subtract" do
-  erb(:subtract)
-end
-
-post "/calculate_subtract" do
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first - second
-  "#{first} - #{second} = #{result}"
-end
-
-get "/multiply" do
-  erb(:multiply)
-end
-
-post "/calculate_multiply" do
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first * second
-  "#{first} * #{second} = #{result}"
-end
-
-get "/divide" do
-  erb(:divide)
-end
-
-post "/calculate_divide" do
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first / second
-  "#{first} / #{second} = #{result}"
-end
-
-
-
-
-
-
-
-# post "/calculate_add" do
-#   "Params data: " + params.inspect
+# get "/add" do
+#   erb(:add)
 # end
 
 # post "/calculate_add" do
 #   first = params[:first_number].to_f
 #   second = params[:second_number].to_f
-#   "#{first} + #{second}"
+#   result = first + second
+#   "#{first} + #{second} = #{result}"
 # end
+
+
+# get "/subtract" do
+#   erb(:subtract)
+# end
+
+# post "/calculate_subtract" do
+#   first = params[:first_number].to_f
+#   second = params[:second_number].to_f
+#   result = first - second
+#   "#{first} - #{second} = #{result}"
+# end
+
+# get "/multiply" do
+#   erb(:multiply)
+# end
+
+# post "/calculate_multiply" do
+#   first = params[:first_number].to_f
+#   second = params[:second_number].to_f
+#   result = first * second
+#   "#{first} * #{second} = #{result}"
+# end
+
+# get "/divide" do
+#   erb(:divide)
+# end
+
+# post "/calculate_divide" do
+#   first = params[:first_number].to_f
+#   second = params[:second_number].to_f
+#   result = first / second
+#   "#{first} / #{second} = #{result}"
+# end
+
+
+
+
+
+
+
