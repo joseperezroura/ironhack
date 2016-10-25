f9bb53f7d2123cb76634fad6b1fcb04547830702
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-10-25 17:59:55 -0400

    server.erb corrections

--- week2/day2/calculator/server.rb
+++ week2/day2/calculator/server.rb
@@ -12,15 +12,28 @@ end
 post "/calculate" do
   first = params[:first_number].to_f
   second = params[:second_number].to_f
-
   
-
+  operation = params["operation"] # => "add, subtract, multiply, divide"
   calc = Calculator.new
 
-  result = calc.add(first, second)
-  result = calc.subtract(first, second)
-  result = calc.multiply(first, second)
-  result = calc.divide(first, second)
+  if params[:operation] == "add"
+  	result = calc.add(first, second)
+  elsif params[:operation] == "subtract"
+  	result = calc.subtract(first, second)
+  elsif params[:operation] == "multiply"
+  	result = calc.multiply(first, second)
+  else params[:operation] == "divide"
+  	result = calc.divide(first, second)
+  end
+  	
+  "#{result}"
+  	
+
+  
+  # result = calc.add(first, second)
+  # result = calc.subtract(first, second)
+  # result = calc.multiply(first, second)
+  # result = calc.divide(first, second)
 
 end
 
