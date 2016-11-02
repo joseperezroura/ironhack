efe823521570ac6e26416c8aa67b39484b6aa057
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-02 10:58:52 -0400

    array for each and counter javascript example

--- week3/day3/javascript/app.js
+++ week3/day3/javascript/app.js
@@ -1,38 +1,66 @@
 // countries = ["puerto Rico", "USA", "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", "Cuba", "Nigeria", "Haiti"]
-var countries = ["puerto Rico", "USA", "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", "Cuba", "Nigeria", "Haiti"];
+// var countries = ["puerto Rico", "USA", "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", "Cuba", "Nigeria", "Haiti"];
+function countCountries (country, array) {
+
+	var counter = 0
+
+	array.forEach(function (eachCountry) {
+		if (eachCountry === country) {
+			counter += 1
+		}
+	});
+	return counter; 
+}
+
+
+var countries = [
+"Puerto Rico", "Puerto Rico",
+"USA", "USA", "USA", "USA", "USA",
+"Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", 
+"Cuba", "Cuba", "Cuba", "Cuba",
+"Nigeria", "Haiti"
+ ];
+
+
+console.log(countCountries("USA",countries) == 5);
+console.log(countCountries("Puerto Rico",countries) == 2);
+console.log(countCountries("Cuba",countries) == 4);
+console.log(countCountries("Nicaragua",countries) == 1);
+console.log(countCountries("Japan",countries) == 0);
+
 
 
 
 // puts " "
 // puts "Countries represented in this room:"
-console.log(" ");
-console.log("Countries represented in this room:");
-console.log("-----------------------------------");
+// console.log(" ");
+// console.log("Countries represented in this room:");
+// console.log("-----------------------------------");
 
 
-// countries.each do |a_country|
-// 	puts "Some of us are from #{a_country}!!"
-// end
-countries.forEach(function (aCountry) {
-console.log("Some of us are from " + aCountry + "!!");
-console.log(`Some of us are from ${aCountry}!!`)
-});
+// // countries.each do |a_country|
+// // 	puts "Some of us are from #{a_country}!!"
+// // end
+// countries.forEach(function (aCountry) {
+// console.log("Some of us are from " + aCountry + "!!");
+// console.log(`Some of us are from ${aCountry}!!`)
+// });
 
 
-// puts " "
-console.log(" ");
+// // puts " "
+// console.log(" ");
 
 
-// uppercase_countries = countries.map do |a_country|
-// 	a_country.uppercase_countries
-// end
-var uppercaseCountries = countries.map(function (aCountry) {
-	return aCountry.toUpperCase();
-});
+// // uppercase_countries = countries.map do |a_country|
+// // 	a_country.uppercase_countries
+// // end
+// var uppercaseCountries = countries.map(function (aCountry) {
+// 	return aCountry.toUpperCase();
+// });
 
-uppercaseCountries.forEach(function (uppercaseCountries) {
-console.log("SOME OF US ARE FROM " + uppercaseCountries + "!!");
-});
+// uppercaseCountries.forEach(function (uppercaseCountries) {
+// console.log("SOME OF US ARE FROM " + uppercaseCountries + "!!");
+// });
 
 // puts"COUNTRIES REPRESENTED IN THIS ROOM (IN UPPERCASE):"
 // puts"---------------------------------------------------"
--- week3/day3/javascript/app.rb
+++ week3/day3/javascript/app.rb
@@ -1,22 +1,56 @@
-countries = ["puerto Rico", "USA", "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", "Cuba", "Nigeria", "Haiti"]
+def count_countries(country, array)
+	counter = 0
 
-puts " "
-puts "Countries represented in this room:"
-
-
-countries.each do |a_country|
-	puts "Some of us are from #{a_country}!!"
+	array.each do |each_country|
+		if each_country == country 
+			counter += 1
+		end
+	end
+	
+	counter
 end
 
-puts " "
 
-uppercase_countries = countries.map do |a_country|
-	a_country.uppercase_countries
-end
 
-puts "COUNTRIES REPRESENTED IN THIS ROOM (IN UPPERCASE):"
-puts"---------------------------------------------------"
+countries = [
+"Puerto Rico", "Puerto Rico",
+ "USA", "USA", "USA", "USA", "USA",
+ "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", 
+ "Cuba", "Cuba", "Cuba", "Cuba",
+ "Nigeria", "Haiti"
+ ]
 
-uppercase_countries.each do |a_country|
-	puts "SOMEO OF US ARE FROM #{a_country}"
-end
+
+p count_countries("USA",countries) == 5
+p count_countries("Puerto Rico",countries) == 2
+p count_countries("Cuba",countries) == 4
+p count_countries("Nicaragua",countries) == 1
+p count_countries("Japan",countries) == 0
+
+
+
+
+
+
+# countries = ["puerto Rico", "USA", "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", "Cuba", "Nigeria", "Haiti"]
+
+# puts " "
+# puts "Countries represented in this room:"
+
+
+# countries.each do |a_country|
+# 	puts "Some of us are from #{a_country}!!"
+# end
+
+# puts " "
+
+# uppercase_countries = countries.map do |a_country|
+# 	a_country.uppercase_countries
+# end
+
+# puts "COUNTRIES REPRESENTED IN THIS ROOM (IN UPPERCASE):"
+# puts"---------------------------------------------------"
+
+# uppercase_countries.each do |a_country|
+# 	puts "SOMEO OF US ARE FROM #{a_country}"
+# end
