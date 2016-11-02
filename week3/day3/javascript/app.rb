0bc866bdbafed3dfaaf70e01b436fec1db580694
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-02 09:42:27 -0400

    set up first javascript files

---
+++ week3/day3/javascript/app.rb
@@ -0,0 +1,11 @@
+countries = ["puerto Rico", "USA", "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", "Cuba", "Nigeria", "Haiti"]
+
+puts " "
+puts "Countries represented in this room:"
+
+
+countries.each do |a_country|
+	puts "Some of us are from #{a_country}!!"
+end
+
+puts " "
