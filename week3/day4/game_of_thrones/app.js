fc477b535801ff84f1b2619bae9315fdfa4c99f1
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-03 16:40:53 -0400

    corrected game of thrones code

--- week3/day4/game_of_thrones/app.js
+++ week3/day4/game_of_thrones/app.js
@@ -1,20 +1,36 @@
 var fs = require('fs');
 
-
-
 function fileActions(err, file){ 
     if (err) {
         throw err;
     }
 	
 	var printEpisodes = JSON.parse(file);
-	var i;
-	for (i = 0; i < printEpisodes.length ; i ++) {
-  		console.log(printEpisodes[i].title);
-  		console.log(`episode number ${printEpisodes[i].episode_number}`);
-  		console.log(printEpisodes[i].description);
 
-  		var roundedRating = Math.round(printEpisodes[i].rating);
+	episodeNumbers = [];
+	for (y = 0; y < printEpisodes.length ; y ++){
+	episodeNumbers.push(printEpisodes[y]);
+	};
+
+
+	var sortedEpisodes = episodeNumbers.sort(function(episode1,episode2){
+	return episode1.episode_number-episode2.episode_number;
+	});
+
+	var filteredEpisodes = [];
+	for (z = 0; z < sortedEpisodes.length; z++){
+		if (sortedEpisodes[z].rating >= 8.5){
+			filteredEpisodes.push(sortedEpisodes[z]);
+		}
+	}
+
+	var i;
+	for (i = 0; i < filteredEpisodes.length ; i ++) {
+  		console.log(filteredEpisodes[i].title);
+  		console.log(`episode number ${filteredEpisodes[i].episode_number}`);
+  		console.log(filteredEpisodes[i].description);
+
+  		var roundedRating = Math.round(filteredEpisodes[i].rating);
   		console.log(`rating ${roundedRating}`);
 
   		var stars = ""
@@ -30,32 +46,3 @@ function fileActions(err, file){
 }
 
 fs.readFile("./episodes.json", 'utf8', fileActions);
-
-
-
-
-
-
-
-
-
-// var fs = require('fs');
-
-
-
-// function fileActions(err, file){ 
-//     if (err) {
-//         throw err;
-//     }
-//     var episodes = JSON.parse(file);
-//     console.log(episodes[3].title)
-
-
-// }
-// fs.readFile("./episodes.json", 'utf8', fileActions);
-
-
-//     // console.log(episodes[0].title);
-//     // console.log(episodes[0].episode_number);
-//     // console.log(episodes[0].description);
-//     // console.log(episodes[0].rating);
