58edaf21ee0c102aba1734f81f261baa3df8ae05
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-03 18:44:11 -0400

    set up vikings

---
+++ week3/day4/vikings/app.js
@@ -0,0 +1,6 @@
+var newPitFight = new PitFight("Thor" , 100, 100)
+var newPitFight = new PitFight("Bjorn" , 100, 100)
+var newPitFight = new PitFight("Gunnr" , 100, 100)
+var newPitFight = new PitFight("Leif" , 100, 100)
+
+
---
+++ week3/day4/vikings/lib/vikings.js
@@ -0,0 +1,10 @@
+class PitFight {
+	constructor (name, health, strength) {
+		this.name = name;
+		this.health = health;
+		this.strength = strength;
+	}
+
+
+
+}
