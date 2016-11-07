fe9a680abad9319559dd05081ef1d10524f0d52d
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-07 17:06:58 -0500

    created results view page with word counter

--- week4/worldcentral/app/controllers/text_inspections_controller.rb
+++ week4/worldcentral/app/controllers/text_inspections_controller.rb
@@ -1,2 +1,24 @@
 class TextInspectionsController < ApplicationController
+def new
+		
+		render 'new' # name of view template
+
+	end
+
+def create
+    @text = params[:text_inspection][:user_text]
+    
+    @word_count = @text.split(" ").length
+
+    render "results"
+
+   
+
+
+  
+  end
 end
+
+
+ActionController::InvalidAuthenticityToken
+
