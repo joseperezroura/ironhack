dbfb59a83bf64e013d4b7170fc9917a33c0b52c9
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-08 16:45:14 -0500

    added ascii method to project

--- week4/worldcentral/app/controllers/asciis_controller.rb
+++ week4/worldcentral/app/controllers/asciis_controller.rb
@@ -1,19 +1,25 @@
 class AsciisController < ApplicationController
-def new		
+
+	def new
+		
 		render 'new'
+	
+	end
+
+	def create
+		
+		x = Artii::Base.new
+		
+		@text = params[:asciis][:user_text]
+		
+		@x = x.asciify(@text)
+		
+		render 'results'
+	end
+
 end
-def create
-    @text = params[:asciis][:asciis_user_text]
-    
-    @word_count = @text.split(" ").length
-
-    render "results"  
-  end
-end
 
 
-
-
-
+ActionController::InvalidAuthenticityToken
 
 
