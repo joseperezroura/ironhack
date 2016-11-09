a8192302ac732ac2576d6df218f33f046fe47097
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-09 11:18:46 -0500

    changed redirect on project controller for create method

--- week4/timetracking/app/controllers/projects_controller.rb
+++ week4/timetracking/app/controllers/projects_controller.rb
@@ -16,5 +16,23 @@ class ProjectsController < ApplicationController
 		render :show
 
 	end
+	def new
+		
+		@my_project = Project.new
+		
+		render :new
+
+	end
+
+	def create
+		@my_project = Project.new(
+			:name => params[:project][:name],
+			:description => params[:project][:description])
+		@my_project.save
+
+		redirect_to project_path(@my_project)
+		# redirect_to '/projects/#{@my_project.id}'
+
+	end
 
 end
--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -10,8 +10,13 @@ Rails.application.routes.draw do
 
   get '/projects', to: 'projects#index'
 
+  get '/projects/new', to: 'projects#new' 
+
   get '/projects/:id', to: 'projects#show', as: 'project'
-  
+
+  post '/projects', to: 'projects#create'
+   
+
 
 
   #<form method="post" action="/calculate"> 
