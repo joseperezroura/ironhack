4700c6f4faa730d570a9b140fc80be9fd1bb6baa
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-10 10:43:25 -0500

    completed new entries controller, view form

--- week4/timetracking/app/models/time_entry.rb
+++ week4/timetracking/app/models/time_entry.rb
@@ -1,3 +1,8 @@
 class TimeEntry < ApplicationRecord
 	belongs_to :project
+	validates :hours, :numericality => true
+	validates :minutes, :numericality => true
+	validates :date, :presence => true
+	
+
 end
--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
 
   post '/projects', to: 'projects#create'
 
+  get '/projects/:project_id/time_entries/new', to: 'time_entries#new', as: "new_project_time_entry"
+
+  post 'projects/:project_id/time_entries', to: 'time_entries#create' 
+
   #<form method="post" action="/calculate"> 
 
 end
