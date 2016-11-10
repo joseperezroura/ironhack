d7aade6b5a5fb746613a827f5256232152c6324c
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-10 16:03:41 -0500

    previous version before full deletion

--- week4/address_book/app/controllers/projects_controller.rb
+++ week4/address_book/app/controllers/projects_controller.rb
@@ -3,7 +3,11 @@ class ProjectsController < ApplicationController
 		def index
 		@projects = Project.all
 		render 'index'
-	end
+		end
+
+		def new
+			render 'new'
+		end
 end
 
 
--- week4/address_book/config/routes.rb
+++ week4/address_book/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'site#home'
   get '/projects', to: 'projects#index'
+  get '/new', to: 'projects#new'
+  post '/projects', to: 'projects#create'
+
 end
