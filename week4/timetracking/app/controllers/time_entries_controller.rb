2bd51f8c59cbdf224ce70929117ac87e6ec9acc5
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-10 14:01:05 -0500

    added partial form file

--- week4/timetracking/app/controllers/time_entries_controller.rb
+++ week4/timetracking/app/controllers/time_entries_controller.rb
@@ -1,47 +1,29 @@
 class TimeEntriesController < ApplicationController
 
 	def index
-
 		@my_project = Project.find(params[:project_id])
-
 		@my_entry = @my_project.time_entries
-
 		render :index
 	end
 
 	def new
-
 		@my_project = Project.find(params[:project_id])
-
 		@my_entry = @my_project.time_entries.new
-
 		render :new
 	end
 
-	def create
-		
-		@my_project = Project.find(params[:project_id])
-		
-		@my_entry = @my_project.time_entries.new(
-			hours: params[:time_entry][:hours],
-			minutes: params[:time_entry][:minutes],
-			date: params[:time_entry][:date],
-			comments: params[:time_entry][:comments])
-
+	def create	
+		@my_project = Project.find(params[:project_id])		
+		@my_entry = @my_project.time_entries.new(entry_params)
 		if @my_entry.save
-
 			redirect_to "/projects/#{@my_project.id}/time_entries"
-
 		else
-
 			render :new
-
 		end
 	end
 
 	def edit
 		@my_project = Project.find params[:project_id]
-
 		@my_entry = @my_project.time_entries.find params[:id]
 
 		render :edit
@@ -50,21 +32,34 @@ class TimeEntriesController < ApplicationController
 
 	def update
 		@my_project = Project.find params[:project_id]
-
 		@my_entry = @my_project.time_entries.find params[:id]
-
-		if @my_entry.update(hours: params[:time_entry][:hours],
-			minutes: params[:time_entry][:minutes],
-			date: params[:time_entry][:date],
-			comments: params[:time_entry][:comments])
-			
+		if @my_entry.update(entry_params)
 		redirect_to "/projects/#{@my_project.id}/time_entries"
-
 		else
-
 		render :edit
-
 		end
+	end
+
+
+	private
+	def entry_params
+		params.require(:time_entry).permit(:hours, :minutes, :date, :comments)
+
+		# params[:time_entry][:hours]
 	end
 end
 
+# (
+# 			hours: params[:time_entry][:hours],
+# 			minutes: params[:time_entry][:minutes],
+# 			date: params[:time_entry][:date],
+# 			comments: params[:time_entry][:comments])
+
+
+
+
+
+# (hours: params[:time_entry][:hours],
+# 			minutes: params[:time_entry][:minutes],
+# 			date: params[:time_entry][:date],
+# 			comments: params[:time_entry][:comments])		
