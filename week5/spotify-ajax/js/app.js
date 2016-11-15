8a4a7ef7aa30569e1a4b2582b0fd82c9e40ab1ff
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-15 17:14:01 -0500

    changed css to terminal look

--- week5/spotify-ajax/js/app.js
+++ week5/spotify-ajax/js/app.js
@@ -2,30 +2,52 @@ console.log("App JS Ready");
 
 $(document).ready(function () {
 
+$(".js-artist-search-form").on("submit", getArtist);
+
+});
 
 
+function getArtist (eventThing) {
+	eventThing.preventDefault();
 
 
+var artistInput = $(".js-artist-search").val();
+
+var searchInfo = {
+		q: artistInput,
+		type: "artist"		
+	};
+$.ajax({
+		type: "GET",
+		url: "https://api.spotify.com/v1/search",
+		data: searchInfo,
+		success: displayArtist,
+		error: handleError,
+	});
+}
+
+function displayArtist (artistInfo) {
+	console.log("success of get artist")
+	console.log(artistInfo);
+
+artistInfo.artists.items.forEach(function (x){
+	if (x.images.length !== 0){
 
 
+	var artistItem = `
+	
+	<li>${x.name}<p>
+	
+	<img src="${x.images[0].url}"></li>`
 
+	$(".js-artist-result").append(artistItem);
 
+};
+});
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function handleError (errorThing) {
+	console.log("Artist search error");
+	console.log(errorThing.responseText);
 }
