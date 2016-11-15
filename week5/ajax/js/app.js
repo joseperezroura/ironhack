5d005130060b0610508954308975b15589e3ca9d
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-15 12:14:07 -0500

    added ackbar form

--- week5/ajax/js/app.js
+++ week5/ajax/js/app.js
@@ -1,11 +1,49 @@
 
-console.log("App JS Ready")
+console.log("App JS Ready");
 
 $(document).ready(function () {
 
 	$(".js-fetch-characters").on("click", fetchCharacters);
+	$(".js-ackbar-form").on("submit", saveAckbar);
 
 });
+
+
+
+function saveAckbar (eventThing) {
+	eventThing.preventDefault();
+
+
+	var weaponInput = $(".js-ackbar-weapon").val();
+
+	if (weaponInput === "") {
+		weaponInput = "Memes";
+	}
+
+
+
+
+	var ackbarInfo = {
+		name: "Admiral Ackbar",
+		occupation: "Rebel Fleet Admiral",
+		weapon: weaponInput,
+	};
+
+	$.ajax({
+		type: "POST",
+		url: "https://ironhack-characters.herokuapp.com/characters",
+		data: ackbarInfo,
+		success: showAckbar,
+		error: handleError,
+	});
+}
+
+
+function showAckbar (infoFromApi) {
+	console.log("Save Ackbar success");
+	console.log(infoFromApi);
+}
+
 
 function fetchCharacters () {
 	$.ajax({
@@ -20,10 +58,19 @@ function showCharacters (x) {
 	console.log("Fetch characters success");
 	console.log(x);
 	
+	$(".js-character-list").empty();
+
 	x.forEach(function (oneCharacter) {
-		
-		$(".js-character-list").append(oneCharacter.name);
-	
+		var characterItem = `
+		<li> 
+		<h3>${oneCharacter.name}</h3>
+		<p> Weapon: ${oneCharacter.weapon} </p> 
+		<p> Occupation: ${oneCharacter.occupation} </p> 
+		<p> Debt: ${oneCharacter.debt} </p> 
+		<p> Id: ${oneCharacter.id} </p> 
+		</li>`;
+
+		$(".js-character-list").append(characterItem);	
 	});
 
 } 
