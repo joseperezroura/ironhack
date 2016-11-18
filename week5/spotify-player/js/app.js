dafc702d251a6aec4b8ed8bf7ac3c42a2b68ce6f
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-18 11:52:52 -0500

    working ajax call to spotify api for songs

--- week5/spotify-player/js/app.js
+++ week5/spotify-player/js/app.js
@@ -1,1 +1,31 @@
 console.log("App Running")
+
+$(document).ready(function () {
+$(".js-song-search-form").on("submit", getSong);
+});
+
+function getSong (eventThing) {
+	eventThing.preventDefault();
+
+ var songInput = $(".js-song-search").val();
+ console.log(songInput)
+
+ $.ajax({
+		type: "GET",
+		url: `https://api.spotify.com/v1/search?type=track&q=${songInput}`,
+		success: displaySong,
+		error: handleError,
+	});	
+
+};
+
+
+
+function displaySong(songInfo){
+
+console.log(songInfo)
+}
+function handleError (errorThing) {
+	console.log("Song search error");
+	console.log(errorThing.responseText);
+}
