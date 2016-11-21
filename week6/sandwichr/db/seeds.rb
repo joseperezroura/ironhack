e7dc66481bf4349c5eb65a72f6f23ba364154dc3
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-21 16:37:21 -0500

    added sandwhichIngredient model

--- week6/sandwichr/db/seeds.rb
+++ week6/sandwichr/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+sandwich1=Sandwich.create!(name: "Grilled Cheese", bread: "White Bread")
+sandwich2=Sandwich.create!(name: "Spicy Chicken", bread: "Roll")
+sandwich3=Sandwich.create!(name: "Salmon Sub", bread: "Wheat Bread")
+
+ingredient1=Ingredient.create!(name: "Mayo", calories: 20)
+ingredient2=Ingredient.create!(name: "Onions", calories: 10)
+ingredient3=Ingredient.create!(name: "Pickles", calories: 5)
