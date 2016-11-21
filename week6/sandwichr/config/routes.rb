8a406f910b7f241ec1986796e998fbedabe8074a
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-21 10:58:33 -0500

    revised routes.rb

--- week6/sandwichr/config/routes.rb
+++ week6/sandwichr/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :sandwiches
+  resources :sandwiches, except: [:new, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
 end
