fb69ebff3e880e721225b5ce55b1228d2afc7757
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-21 16:56:42 -0500

    added seeds to db

--- week6/sandwichr/db/seeds.rb
+++ week6/sandwichr/db/seeds.rb
@@ -5,10 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-sandwich1=Sandwich.create!(name: "Grilled Cheese", bread: "White Bread")
-sandwich2=Sandwich.create!(name: "Spicy Chicken", bread: "Roll")
-sandwich3=Sandwich.create!(name: "Salmon Sub", bread: "Wheat Bread")
+sandwich1=Sandwich.create!(name: "Grilled Cheese", bread_type: "White")
+sandwich2=Sandwich.create!(name: "Spicy Chicken", bread_type: "Roll")
+sandwich3=Sandwich.create!(name: "Salmon Sub", bread_type: "Wheat Bread")
 
 ingredient1=Ingredient.create!(name: "Mayo", calories: 20)
 ingredient2=Ingredient.create!(name: "Onions", calories: 10)
 ingredient3=Ingredient.create!(name: "Pickles", calories: 5)
+
+SandwichIngredient.create!(sandwich_id: sandwich1.id, ingredient_id: ingredient1.id)
+SandwichIngredient.create!(sandwich_id: sandwich1.id, ingredient_id: ingredient2.id)
+SandwichIngredient.create!(sandwich_id: sandwich1.id, ingredient_id: ingredient3.id)
+SandwichIngredient.create!(sandwich_id: sandwich2.id, ingredient_id: ingredient1.id)
+SandwichIngredient.create!(sandwich_id: sandwich2.id, ingredient_id: ingredient2.id)
+SandwichIngredient.create!(sandwich_id: sandwich2.id, ingredient_id: ingredient3.id)
+SandwichIngredient.create!(sandwich_id: sandwich3.id, ingredient_id: ingredient1.id)
+SandwichIngredient.create!(sandwich_id: sandwich3.id, ingredient_id: ingredient2.id)
+SandwichIngredient.create!(sandwich_id: sandwich3.id, ingredient_id: ingredient3.id)
