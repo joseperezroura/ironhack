bd9d63923a9c89d36edff9f2d137d0f1983a403f
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-21 11:43:26 -0500

    added create and show methods in controller

--- week6/sandwichr/app/controllers/sandwiches_controller.rb
+++ week6/sandwichr/app/controllers/sandwiches_controller.rb
@@ -1,2 +1,31 @@
 class SandwichesController < ApplicationController
+	protect_from_forgery with: :null_session
+
+def index
+	sandwiches = Sandwich.all
+	render json: sandwiches
+end
+
+def create
+	sandwich = Sandwich.create(sandwich_params)
+	render json: sandwich
+end
+
+def show
+	sandwich = Sandwich.find(params[:id])
+	render json: sandwich 
+end
+
+
+
+
+
+private
+
+def sandwich_params
+	params.require(:sandwich)
+	.permit(:name, :bread_type)
+end
+
+
 end
