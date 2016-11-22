cd01035c383fd8555f6c7c60cf4d07e5cd588e1b
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-22 17:15:25 -0500

    iteration 1/Sandwichr Day2

--- week6/sandwichr/config/routes.rb
+++ week6/sandwichr/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  
+resources :sandwiches, only: [:index, :show], controller: "sandwich_views"
+	scope "/api" do
   post "sandwiches/:id/ingredients/add", to: "sandwiches#add_ingredient"
   delete "sandwiches/:id/ingredients/remove", to: "sandwiches#remove_ingredient"
   resources :ingredients, except: [:new, :edit]
   resources :sandwiches, except: [:new, :edit]
+    end
+
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
