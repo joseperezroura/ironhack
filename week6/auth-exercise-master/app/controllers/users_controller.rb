1a80bed5febbfefda1ca2028b254e57f860c940a
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-22 14:13:13 -0500

    pages are strict

--- week6/auth-exercise-master/app/controllers/users_controller.rb
+++ week6/auth-exercise-master/app/controllers/users_controller.rb
@@ -3,40 +3,46 @@ class UsersController < ApplicationController
   # renders the home page
   def home
      get_current_user
-  render :home
+      render :home
   end
 
   def index
-    @users = User.all
+      if session[:user_id]
+      @users = User.all
+
+      render :index
+      else
+     redirect_to "/login"
+    end
   end
 
   # renders the signup form
   def new
   end
 
-  def show
-     get_current_user
-  render :show
+    def show
+       if session[:user_id]
+        get_current_user
+        render :show
+      else
+        redirect_to "/login"
   end
-
+  end
   # receives form and creates a user from that data
-  def create
+    def create
     user = User.new(user_params)
-    if user.save
+      if user.save
       redirect_to '/'
-    else
+      else
       redirect_to '/signup'
-    end
-  end   
+      end
+    end   
 
   private
 
   def user_params
      params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
-
-  
-
 
   def user_logged_in?
     if session[:user_id]
