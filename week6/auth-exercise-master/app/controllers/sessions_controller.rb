1ea7482301d3890ac3acb27d6da74d9725a9aa74
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-22 14:00:23 -0500

    created working logout/destroy method in user controller

--- week6/auth-exercise-master/app/controllers/sessions_controller.rb
+++ week6/auth-exercise-master/app/controllers/sessions_controller.rb
@@ -4,4 +4,42 @@ def new
 	render :new
 end
 
+def create
+	user_maybe = User.find_by(email: params[:email])
+
+	if 
+		user_maybe == nil
+		
+		redirect_to "/login"
+	
+	elsif 
+		user_maybe.authenticate(params[:password]) == false
+		
+		redirect_to "/login"
+	
+	else 
+		session[:user_id] = user_maybe.id 
+		
+		redirect_to "/"
+	
+	end
 end
+
+def destroy
+
+	# session[;user_id] = nil
+
+	session.clear
+
+	redirect_to "/"
+
+end
+
+end
+
+# if user_maybe && user_maybe.authenticate(params[:password])
+# 	session[:user_id] = user_maybe.id
+# 	redirect_to "/"
+# else
+# 	redirect_to "/login"
+# end
--- week6/auth-exercise-master/app/controllers/users_controller.rb
+++ week6/auth-exercise-master/app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
 
   # renders the home page
-def home
-  @name = current_user ? @current_user.username : "Ironhacker"
-end
+  def home
+     get_current_user
+  render :home
+  end
 
   def index
     @users = User.all
@@ -14,6 +15,8 @@ end
   end
 
   def show
+     get_current_user
+  render :show
   end
 
   # receives form and creates a user from that data
@@ -32,4 +35,15 @@ end
      params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 
+  
+
+
+  def user_logged_in?
+    if session[:user_id]
+      true
+    else
+      false
+    end
+  end
 end
+
