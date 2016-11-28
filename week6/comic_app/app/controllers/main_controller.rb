37cbb863f384d18a0b83dd8a260cf50d411e9e85
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-11-28 12:43:09 -0500

    created users show page

--- week6/comic_app/app/controllers/main_controller.rb
+++ week6/comic_app/app/controllers/main_controller.rb
@@ -1,6 +1,8 @@
 class MainController < ApplicationController
 
 def home
+	@users_list = User.all
+	
 	render :home
 end
 
---
+++ week6/comic_app/app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+def show
+	authenticate_user!
+
+
+	@the_user = User.find(params[:id])
+	@comics_list = @the_user.comics
+	render :show
+end
+
+
+end
