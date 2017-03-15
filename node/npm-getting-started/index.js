c2554df63c119e4984bff6bd37fee3cd37134eaa
Jose Perez-Roura <jose.perezroura@gmail.com>
2017-03-15 14:19:12 -0400

    node js test

--- node/npm-getting-started/index.js
+++ node/npm-getting-started/index.js
@@ -1,3 +1,9 @@
-const cows = require('cows');
+var age = parseInt(prompt("Welcome to Ironhack cinema. How old are you?"));
 
-console.log(cows())
+if (age <= 16) {
+  console.log ("You have a teenager discount");
+} else if (age >= 65) {
+  console.log ("You have the senior citizen discount :)");
+} else {
+  console.log ("Sorry, you have no discount :(");
+}
